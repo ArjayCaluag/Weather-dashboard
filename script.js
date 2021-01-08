6cbc5688acd62c4ae28a61b7e085df005c0e0210
Ron-Arjay Caluag <arjaycaluag@gmail.com>
2021-01-07 18:30:28 -0800

    Dynamically create new button with search history input field

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {
-
-
-    function searchCity(cities) {
         let apiKey = '&appid=b540f485d7fc2dc82c2bec349be77cf3';
+    function searchCity(cities) {
+       
         let queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" + cities + '&units=imperial' + apiKey;
         let newDiv = $('<div>')
 
@@ -23,36 +22,37 @@ $(document).ready(function () {
 
             // appends all of our dynamic html to our currentWeather div tag
             newDiv.append(cityName, cityTemp, cityHumid, citySpeed)
+            
             $('#currentWeather').append(newDiv)
         })
     }
 
+    historyButton();
+   
     // event listener onClick - perform this work
     $('#searchCity').on("click", function (event) {
         // stops the page from trying to submit a form
         event.preventDefault();
         // Grabs the value from input field id citySearch and places as an argument in our function.
         let inputCity = $("#citySearch").val().trim();
+        
+        // Set input into local storage. key|value 'city|historyArray
+        var storeInput = $(this).siblings("input").val();
+        var historyArray = [];
+        historyArray.push(storeInput);
+        localStorage.setItem('city', JSON.stringify(historyArray));
+        
+        
         searchCity(inputCity)
-    })
+        historyButton();
+    });
 
+    // function creates new button
+    function historyButton(){
+        let getStorage = JSON.parse(localStorage.getItem('city'));
+        let newButton = $("<button>").addClass('row btn btn-primary').text(getStorage);
+        $('#historyButton').append(newButton);
 
-
-    // function searchCity(cityName){
-    //     for(i=0;i<cityName.length;i++){
-    //         let newButton = $('<button');
-    //         newButton.attr(cityName[i]);
-    //         newButton.text(cityName[i])
-    //         $('#historyButton').append(newButton);
-
-    //     }
-    // }
-    // searchCity()
-    // Create queryURL var that contains weathermap api + user input city name + API key
-
-    // Import weathermap API with Ajax to call for response and test with consolelog
-
-    // Create on("click") event listener for submit button, that then links to our queryURL key to present information about selected city
-    // In addition, create dynamic buttons that uses localstorage to pull up same information if pressed again
-
+    }
+   
 });
